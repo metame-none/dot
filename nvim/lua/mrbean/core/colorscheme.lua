944ce4fa058277bd9d9362ba8e53be365b38e36d
metame <metame.none@gmail.com>
2024-04-01 00:09:03 +0800

    update

--- nvim/lua/mrbean/core/colorscheme.lua
+++ nvim/lua/mrbean/core/colorscheme.lua
@@ -1,9 +1,9 @@
-vim.cmd("colorscheme molokai")
+-- vim.cmd("colorscheme molokai")
+-- vim.g.molokai_original = 1
 
-vim.g.molokai_original = 1
-
--- local status, _ = pcall(vim.cmd, "colorscheme nightfly")
--- if not status then
---   print("color scheme not found")
---   return
--- end
+vim.cmd("colorscheme nightfly")
+local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+if not status then
+  print("color scheme not found")
+  return
+end
